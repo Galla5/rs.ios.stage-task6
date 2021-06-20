ff2c1adb330902fdfee28979950c877b0f86cab7
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-20 15:24:49 +0300

    feat: update coronaclass solution.

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -2,17 +2,29 @@ import Foundation
 
 class CoronaClass {
  
+     var studentsCount: Int = 0
      var seats = [Int]()
+     let desksCount: Int
 
      init(n: Int) {
- 
+        self.desksCount = n
      }
      
      func seat() -> Int {
-        -1
+        if studentsCount == 0 {
+            studentsCount += 1
+            seats.append(0)
+            return 0
+        }
+        return 0
      }
      
      func leave(_ p: Int) {
-         
+         guard seats.contains(p) else { return }
+         studentsCount -= 1
+         if let indexP = seats.firstIndex(of: p) {
+            seats.remove(at: indexP)
+         }
      }
 }
+
