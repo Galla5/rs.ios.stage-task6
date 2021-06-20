7b7b0b8cf05dfc8f979c62dcc63862e8662fff08
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-20 16:27:04 +0300

    feature: update coronaclass seat.

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -16,8 +16,39 @@ class CoronaClass {
             seats.append(0)
             return 0
         }
-        return 0
+        if studentsCount == 1 {
+            studentsCount += 1
+            seats.append(desksCount - 1)
+            return desksCount - 1
+        }
+        studentsCount += 1
+        let result = distanse()
+        let studentDisptance = result.0 / 2
+        let startSeat =  result.1
+        let delta = startSeat == 0 && studentsCount == 3 ? 1 : 0
+        let newStudentSeat = studentDisptance + startSeat + delta
+        seats.append(newStudentSeat)
+        seats.sort(by: { $0 < $1})
+        return newStudentSeat
      }
+    
+    func distanse() -> (Int, Int) {
+        var newSeats = seats
+        var resultDistanse: Int = 0
+        var startSeat: Int = 0
+        for (index, seat) in newSeats.enumerated() {
+            if index == newSeats.count - 1 {
+                break
+            }
+            let nextSeat = newSeats[index + 1]
+            let distanse = nextSeat - seat
+            if distanse > resultDistanse {
+                resultDistanse = distanse
+                startSeat = seat
+            }
+        }
+        return (resultDistanse, startSeat)
+    }
      
      func leave(_ p: Int) {
          guard seats.contains(p) else { return }
